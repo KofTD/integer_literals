3376734eccfdf9a50dd7a72413a8a0cfcebe9bc4
Daniel Nesterov <KOfTD@yandex.ru>
2025-05-29 21:35:03 +0300

    Return consteval functions

--- integer_literals.cpp
+++ integer_literals.cpp
@@ -1,6 +1,6 @@
 #include <integer_literals.hpp>
 
-// #ifndef __cpp_consteval
+#ifndef __cpp_consteval
 
 constexpr std::uint8_t
 integer_literals::operator""_u8(unsigned long long value) {
@@ -82,86 +82,86 @@ integer_literals::operator""_if64(unsigned long long value) {
   return static_cast<std::int_fast64_t>(value);
 }
 
-// #else
-//
-// consteval std::uint8_t
-// integer_literals::operator""_u8(unsigned long long value) {
-//   return static_cast<std::uint8_t>(value);
-// }
-//
-// consteval std::uint16_t
-// integer_literals::operator""_u16(unsigned long long value) {
-//   return static_cast<std::uint16_t>(value);
-// }
-//
-// consteval std::uint32_t
-// integer_literals::operator""_u32(unsigned long long value) {
-//   return static_cast<std::uint32_t>(value);
-// }
-//
-// consteval std::uint64_t
-// integer_literals::operator""_u64(unsigned long long value) {
-//   return static_cast<std::uint64_t>(value);
-// }
-//
-// consteval std::int8_t
-// integer_literals::operator""_i8(unsigned long long value) {
-//   return static_cast<std::int8_t>(value);
-// }
-//
-// consteval std::int16_t
-// integer_literals::operator""_i16(unsigned long long value) {
-//   return static_cast<std::int16_t>(value);
-// }
-//
-// consteval std::int32_t
-// integer_literals::operator""_i32(unsigned long long value) {
-//   return static_cast<std::int32_t>(value);
-// }
-//
-// consteval std::int64_t
-// integer_literals::operator""_i64(unsigned long long value) {
-//   return static_cast<std::int64_t>(value);
-// }
-//
-// consteval std::int_least8_t
-// integer_literals::operator""_il8(unsigned long long value) {
-//   return static_cast<std::int_least8_t>(value);
-// }
-//
-// consteval std::int_least16_t
-// integer_literals::operator""_il16(unsigned long long value) {
-//   return static_cast<std::int_least16_t>(value);
-// }
-//
-// consteval std::int_least32_t
-// integer_literals::operator""_il32(unsigned long long value) {
-//   return static_cast<std::int_least32_t>(value);
-// }
-//
-// consteval std::int_least64_t
-// integer_literals::operator""_il64(unsigned long long value) {
-//   return static_cast<std::int_least64_t>(value);
-// }
-//
-// consteval std::int_fast8_t
-// integer_literals::operator""_if8(unsigned long long value) {
-//   return static_cast<std::int_fast8_t>(value);
-// }
-//
-// consteval std::int_fast16_t
-// integer_literals::operator""_if16(unsigned long long value) {
-//   return static_cast<std::int_fast16_t>(value);
-// }
-//
-// consteval std::int_fast32_t
-// integer_literals::operator""_if32(unsigned long long value) {
-//   return static_cast<std::int_fast32_t>(value);
-// }
-//
-// consteval std::int_fast64_t
-// integer_literals::operator""_if64(unsigned long long value) {
-//   return static_cast<std::int_fast64_t>(value);
-// }
-//
-// #endif
+#else
+
+consteval std::uint8_t
+integer_literals::operator""_u8(unsigned long long value) {
+  return static_cast<std::uint8_t>(value);
+}
+
+consteval std::uint16_t
+integer_literals::operator""_u16(unsigned long long value) {
+  return static_cast<std::uint16_t>(value);
+}
+
+consteval std::uint32_t
+integer_literals::operator""_u32(unsigned long long value) {
+  return static_cast<std::uint32_t>(value);
+}
+
+consteval std::uint64_t
+integer_literals::operator""_u64(unsigned long long value) {
+  return static_cast<std::uint64_t>(value);
+}
+
+consteval std::int8_t
+integer_literals::operator""_i8(unsigned long long value) {
+  return static_cast<std::int8_t>(value);
+}
+
+consteval std::int16_t
+integer_literals::operator""_i16(unsigned long long value) {
+  return static_cast<std::int16_t>(value);
+}
+
+consteval std::int32_t
+integer_literals::operator""_i32(unsigned long long value) {
+  return static_cast<std::int32_t>(value);
+}
+
+consteval std::int64_t
+integer_literals::operator""_i64(unsigned long long value) {
+  return static_cast<std::int64_t>(value);
+}
+
+consteval std::int_least8_t
+integer_literals::operator""_il8(unsigned long long value) {
+  return static_cast<std::int_least8_t>(value);
+}
+
+consteval std::int_least16_t
+integer_literals::operator""_il16(unsigned long long value) {
+  return static_cast<std::int_least16_t>(value);
+}
+
+consteval std::int_least32_t
+integer_literals::operator""_il32(unsigned long long value) {
+  return static_cast<std::int_least32_t>(value);
+}
+
+consteval std::int_least64_t
+integer_literals::operator""_il64(unsigned long long value) {
+  return static_cast<std::int_least64_t>(value);
+}
+
+consteval std::int_fast8_t
+integer_literals::operator""_if8(unsigned long long value) {
+  return static_cast<std::int_fast8_t>(value);
+}
+
+consteval std::int_fast16_t
+integer_literals::operator""_if16(unsigned long long value) {
+  return static_cast<std::int_fast16_t>(value);
+}
+
+consteval std::int_fast32_t
+integer_literals::operator""_if32(unsigned long long value) {
+  return static_cast<std::int_fast32_t>(value);
+}
+
+consteval std::int_fast64_t
+integer_literals::operator""_if64(unsigned long long value) {
+  return static_cast<std::int_fast64_t>(value);
+}
+
+#endif
--- integer_literals.hpp
+++ integer_literals.hpp
@@ -5,7 +5,7 @@
 
 namespace integer_literals {
 
-// #ifndef __cpp_consteval
+#ifndef __cpp_consteval
 
 constexpr std::uint8_t operator""_u8(unsigned long long value);
 
@@ -39,41 +39,41 @@ constexpr std::int_fast32_t operator""_if32(unsigned long long value);
 
 constexpr std::int_fast64_t operator""_if64(unsigned long long value);
 
-// #else
-//
-// consteval std::uint8_t operator""_u8(unsigned long long value);
-//
-// consteval std::uint16_t operator""_u16(unsigned long long value);
-//
-// consteval std::uint32_t operator""_u32(unsigned long long value);
-//
-// consteval std::uint64_t operator""_u64(unsigned long long value);
-//
-// consteval std::int8_t operator""_i8(unsigned long long value);
-//
-// consteval std::int16_t operator""_i16(unsigned long long value);
-//
-// consteval std::int32_t operator""_i32(unsigned long long value);
-//
-// consteval std::int64_t operator""_i64(unsigned long long value);
-//
-// consteval std::int_least8_t operator""_il8(unsigned long long value);
-//
-// consteval std::int_least16_t operator""_il16(unsigned long long value);
-//
-// consteval std::int_least32_t operator""_il32(unsigned long long value);
-//
-// consteval std::int_least64_t operator""_il64(unsigned long long value);
-//
-// consteval std::int_fast8_t operator""_if8(unsigned long long value);
-//
-// consteval std::int_fast16_t operator""_if16(unsigned long long value);
-//
-// consteval std::int_fast32_t operator""_if32(unsigned long long value);
-//
-// consteval std::int_fast64_t operator""_if64(unsigned long long value);
-//
-// #endif
+#else
+
+consteval std::uint8_t operator""_u8(unsigned long long value);
+
+consteval std::uint16_t operator""_u16(unsigned long long value);
+
+consteval std::uint32_t operator""_u32(unsigned long long value);
+
+consteval std::uint64_t operator""_u64(unsigned long long value);
+
+consteval std::int8_t operator""_i8(unsigned long long value);
+
+consteval std::int16_t operator""_i16(unsigned long long value);
+
+consteval std::int32_t operator""_i32(unsigned long long value);
+
+consteval std::int64_t operator""_i64(unsigned long long value);
+
+consteval std::int_least8_t operator""_il8(unsigned long long value);
+
+consteval std::int_least16_t operator""_il16(unsigned long long value);
+
+consteval std::int_least32_t operator""_il32(unsigned long long value);
+
+consteval std::int_least64_t operator""_il64(unsigned long long value);
+
+consteval std::int_fast8_t operator""_if8(unsigned long long value);
+
+consteval std::int_fast16_t operator""_if16(unsigned long long value);
+
+consteval std::int_fast32_t operator""_if32(unsigned long long value);
+
+consteval std::int_fast64_t operator""_if64(unsigned long long value);
+
+#endif
 
 } // namespace integer_literals
 
